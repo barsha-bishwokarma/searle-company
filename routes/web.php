c514a47e057c7249877925018c0ff585850e0b56
barsha-bishwokarma <bishwokarmabarsha49@gmail.com>
2026-05-01 22:53:03 +0545

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\AboutController;
 use App\Http\Controllers\Frontend\BrandController;
 use App\Http\Controllers\Frontend\CategoryController;
 use App\Http\Controllers\Frontend\ContactController;
@@ -7,7 +8,6 @@ use App\Http\Controllers\Frontend\FacilityController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\InvestorController;
 use App\Http\Controllers\Frontend\NewsController;
-use App\Http\Controllers\Frontend\PageController;
 use App\Http\Controllers\Frontend\PartnerController;
 use App\Http\Controllers\Frontend\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -18,7 +18,18 @@ use Illuminate\Support\Facades\Route;
 
 // 
 Route::get('/',  [HomeController::class, 'home'])->name('home');
-Route::get('/about',  [PageController::class, 'about'])->name('about');
+Route::get('/about',  [AboutController::class, 'about'])->name('about');
+Route::prefix('about')->name('about.')->group(function () {
+    Route::get('/ceo',        [AboutController::class, 'ceo'])       ->name('ceo');
+    Route::get('/ibl',        [AboutController::class, 'ibl'])       ->name('ibl');
+    Route::get('/history',    [AboutController::class, 'history'])   ->name('history');
+    Route::get('/mission',    [AboutController::class, 'mission'])   ->name('mission');
+    Route::get('/facilities', [AboutController::class, 'facilities'])->name('facilities');
+    Route::get('/csr',        [AboutController::class, 'csr'])       ->name('csr');
+    Route::get('/global',     [AboutController::class, 'global'])    ->name('global');
+    Route::get('/policies',   [AboutController::class, 'policies'])  ->name('policies');
+});
+
 
 // Facilities 
 Route::get('/facilities/{slug}', [FacilityController::class, 'show'])->name('facilities.show');
@@ -26,14 +37,13 @@ Route::get('/facilities/{slug}', [FacilityController::class, 'show'])->name('fac
 // Brands
 Route::get('/brands', [BrandController::class, 'index'])->name('brands.index');
 
+// Products
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{slug}', [ProductController::class, 'show'])->name('products.show');
+
 // Categories
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
-
-// Products
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-
 
 // News
 Route::get('/news', [NewsController::class, 'index'])->name('news.index');
