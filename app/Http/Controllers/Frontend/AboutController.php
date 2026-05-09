64bb15b65409ce7a2961927d8b995741cbe1d38c
barsha-bishwokarma <bishwokarmabarsha49@gmail.com>
2026-05-09 08:14:03 +0545

    update controllers logic

--- app/Http/Controllers/Frontend/AboutController.php
+++ app/Http/Controllers/Frontend/AboutController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Facility;
 
 class AboutController extends Controller
 {
-     
+
     public function ceo()
     {
         return view('frontend.about.ceo');
@@ -29,9 +30,11 @@ class AboutController extends Controller
 
     public function facilities()
     {
-        return view('frontend.about.facilities');
-    }
+        $facility = Facility::first();   // for hero image
+        $facilities = Facility::all();   // for accordion list
 
+        return view('Frontend.about.facilities', compact('facility', 'facilities'));
+    }
     public function csr()
     {
         return view('frontend.about.csr');
